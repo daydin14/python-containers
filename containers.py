26b6fc9c6f5542a1ee08f4525be98e7194733a0f
David Aydin <david.aydin6@gmail.com>
2022-06-29 23:06:24 -0400

    Exercise 1, create list and print second and last student

--- containers.py
+++ containers.py
@@ -1,2 +1,4 @@
-students = ['David', 'Arnaud', 'Iyana', 'Davis', 'Jquan', 'Matt', 'Danny', 'Irving', 'Eric']
+students = ['David', 'Arnaud', 'Iyana', 'Davis',
+            'Jquan', 'Matt', 'Danny', 'Irving', 'Eric']
 print(f'Second student: {students[1]}')
+print(f'Last student: {students[len(students)-1]}')
