65fdffe309ef0eaa7bf6d8627ef2f604b0f93af3
David Aydin <david.aydin6@gmail.com>
2022-06-29 23:22:38 -0400

    Exercise 2: create tuple and print the tuple

--- containers.py
+++ containers.py
@@ -1,4 +1,14 @@
+# Exercise 1
 students = ['David', 'Arnaud', 'Iyana', 'Davis',
             'Jquan', 'Matt', 'Danny', 'Irving', 'Eric']
+# ^ This is a list
+print('\n ### Exercise 1: \n')
 print(f'Second student: {students[1]}')
 print(f'Last student: {students[len(students)-1]}')
+
+# Exercise 2
+print('\n ### Exercise 2: \n')
+foods = ('Orange', 'Grapefruit', 'Cherry', 'Peach', 'Lemon',
+         'Lime', 'Blueberry', 'Watermelon', 'Grapes',)
+for food in foods:
+    print(f'{food} is a good food.')
