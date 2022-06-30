4c9da6411fe1ecb5998098bd04be146a56cc8908
David Aydin <david.aydin6@gmail.com>
2022-06-30 00:06:49 -0400

    Exercise 4: create dictionary and print its contents based off desired format

--- containers.py
+++ containers.py
@@ -19,3 +19,18 @@ print('\n ### Exercise 3: \n')
 for food in foods:
     if food == foods[-1] or food == foods[-2]:
         print(food)
+# This conditional and print statment can be placed in the above for loop from exercise 2
+
+# Exercise 4
+print('\n ### Exercise 4: \n')
+home_town = {
+    'city': 'my home',
+    'state': 'jersey',
+    'population': '6',
+}
+# ^ This is a dictionary
+print(
+    f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}")
+
+# Exercise 5
+print('\n ### Exercise 5: \n')
